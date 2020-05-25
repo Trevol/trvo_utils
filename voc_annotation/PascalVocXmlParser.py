ca6129e944a879a2b46003c3914d4fda5c015dcc
trevol <iamvovan@mail.ru>
2020-05-25 15:01:51 +0300

    voc annotation parser

---
+++ voc_annotation/PascalVocXmlParser.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+from xml.etree.ElementTree import parse
+
+
+class PascalVocXmlParser(object):
+    """Parse annotation for 1-annotation file """
+
+    def __init__(self, annFile):
+        self._root = self._root_tag(annFile)
+
+    def filename(self):
+        """
+        # Args
+            annotation_file : str
+                annotation file including directory path
+
+        # Returns
+            filename : str
+        """
+        return self._root.find("filename").text
+
+    def labels(self):
+        """
+        # Args
+            annotation_file : str
+                annotation file including directory path
+
+        # Returns
+            labels : list of strs
+        """
+        labels = []
+        obj_tags = self._root.findall("object")
+        for t in obj_tags:
+            labels.append(t.find("name").text)
+        return labels
+
+    def boxes(self):
+        """
+        # Args
+            annotation_file : str
+                annotation file including directory path
+
+        # Returns
+            bbs : 2d-array, shape of (N, 4)
+                (x1, y1, x2, y2)-ordered
+        """
+        bbs = []
+        obj_tags = self._root.findall("object")
+        for t in obj_tags:
+            box_tag = t.find("bndbox")
+            x1 = box_tag.find("xmin").text
+            y1 = box_tag.find("ymin").text
+            x2 = box_tag.find("xmax").text
+            y2 = box_tag.find("ymax").text
+            box = np.float32([x1, y1, x2, y2])
+            bbs.append(box)
+        bbs = np.array(bbs)
+        return bbs
+
+    @staticmethod
+    def _root_tag(fname):
+        tree = parse(fname)
+        root = tree.getroot()
+        return root
