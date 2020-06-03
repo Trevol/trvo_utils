e6699f2b733af451dc83caec1f20f9cc7ea443cf
trevol <iamvovan@mail.ru>
2020-06-03 18:40:39 +0300

    glob_files

--- trvo_utils/__init__.py
+++ trvo_utils/__init__.py
@@ -1,2 +1,11 @@
+from glob import glob
+
+
 def toInt(*args):
     return map(int, args)
+
+
+def glob_files(patterns):
+    for p in patterns:
+        for f in sorted(glob(p)):
+            yield f
